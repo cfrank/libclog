5e8f7eba7b81db892cfc934c4ce2a2900730a24d
Chris Frank <chris@cfrank.org>
2018-06-10 22:53:25 -0400

    start header lol

--- include/clog.h
+++ include/clog.h
@@ -1,1 +1,13 @@
-void log(const char *message);
+#ifndef CLOG_H
+#define CLOG_H
+
+enum LogLevel {
+        LOG_TRACE = 312157,
+        LOG_DEBUG,
+        LOG_INFO,
+        LOG_WARNING,
+        LOG_ERROR,
+        LOG_CRITICAL
+}
+
+#endif /* CLOG_H */
