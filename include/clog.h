a1b1e55c3cf89f3a6012ffb6567dd89b559197ed
Chris Frank <chris@cfrank.org>
2018-06-16 02:00:54 -0400

    start working on logic, naming, and fix build

--- include/clog.h
+++ include/clog.h
@@ -5,8 +5,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef void(*output_logger_func_t)(const char *message);
-typedef void(*error_handler_func_t)(int error_status);
+typedef void (*output_func_t)(const char *message);
+typedef void (*error_func_t)(int error_status);
 
 enum log_level {
         LEVEL_TRACE = 312157,
@@ -18,46 +18,49 @@ enum log_level {
 };
 
 struct logger {
-        enum log_level display_level;
-        error_handler_func_t error_handler;
+        enum log_level max_level;
+        error_func_t error_handler_func;
         pthread_mutex_t *lock;
-        output_logger_func_t output_logger;
+        output_func_t output_logger_func;
         bool quiet;
 };
 
 struct logger *create_logger(void);
 void destroy_logger(struct logger *log);
-void set_logging_display_level(struct logger *log, enum log_level level);
-void set_error_handler_func(struct logger *log, error_handler_func_t error_handler);
-void set_output_logger_func(struct logger *log, output_logger_func_t output_logger);
-void set_quiet(bool quiet);
-void internal_logger(struct logger *log, enum log_level level, const char *file,
-        uint32_t line, const char *fmt, ...);
-void internal_logger_short(struct logger *log, enum log_level level, const char* fmt, ...);
+void set_logging_max_level(struct logger *log, enum log_level level);
+void set_error_func(struct logger *log, error_func_t error_handler_func);
+void set_output_func(struct logger *log, output_func_t output_logger_func);
+void set_quiet(struct logger *log, bool quiet);
+void internal_logger(const struct logger *log, enum log_level level,
+                     const char *file, uint32_t line, const char *fmt, ...);
+void internal_logger_short(const struct logger *log, enum log_level level,
+                           const char *fmt, ...);
 
-#define LOG_TRACE(log, fmt, ...) \
+#define LOG_TRACE(log, fmt, ...)                                               \
         internal_logger(log, LEVEL_TRACE, __FILE__, __LINE__, fmt, __VA_ARGS__);
-#define LOG_TRACE_SHORT(log, fmt, ...) \
+#define LOG_TRACE_SHORT(log, fmt, ...)                                         \
         internal_logger_short(log, LEVEL_TRACE, fmt, __VA_ARGS__);
-#define LOG_DEBUG(log, fmt, ...) \
+#define LOG_DEBUG(log, fmt, ...)                                               \
         internal_logger(log, LEVEL_DEBUG, __FILE__, __LINE__, fmt, __VA_ARGS__);
-#define LOG_DEBUG_SHORT(log, fmt, ...) \
+#define LOG_DEBUG_SHORT(log, fmt, ...)                                         \
         internal_logger_short(log, LEVEL_DEBUG, fmt, __VA_ARGS__);
-#define LOG_INFO(log, fmt, ...) \
+#define LOG_INFO(log, fmt, ...)                                                \
         internal_logger(log, LEVEL_INFO, __FILE__, __LINE__, fmt, __VA_ARGS__);
-#define LOG_INFO_SHORT(log, fmt, ...) \
+#define LOG_INFO_SHORT(log, fmt, ...)                                          \
         internal_logger(log, LEVEL_INFO, fmt, __VA_ARGS__);
-#define LOG_WARNING(log, fmt, ...) \
-        internal_logger(log, LEVEL_WARNING, __FILE__, __LINE__, fmt, __VA_ARGS__);
-#define LOG_WARNING_SHORT(log, fmt, ...) \
+#define LOG_WARNING(log, fmt, ...)                                             \
+        internal_logger(                                                       \
+                log, LEVEL_WARNING, __FILE__, __LINE__, fmt, __VA_ARGS__);
+#define LOG_WARNING_SHORT(log, fmt, ...)                                       \
         internal_logger_short(log, LEVEL_WARNING, fmt, __VA_ARGS__);
-#define LOG_ERROR(log, fmt, ...) \
+#define LOG_ERROR(log, fmt, ...)                                               \
         internal_logger(log, LEVEL_ERROR, __FILE__, __LINE__, fmt, __VA_ARGS__);
-#define LOG_ERROR_SHORT(log, fmt, ...) \
+#define LOG_ERROR_SHORT(log, fmt, ...)                                         \
         internal_logger_short(log, LEVEL_ERROR, fmt, __VA_ARGS__);
-#define LOG_CRITICAL(log, fmt, ...) \
-        internal_logger(log, LOG_CRITICAL, __FILE__, __LINE__, fmt, __VA_ARGS__);
-#define LOG_CRITICAL_SHORT(log, fmt, ...) \
+#define LOG_CRITICAL(log, fmt, ...)                                            \
+        internal_logger(                                                       \
+                log, LOG_CRITICAL, __FILE__, __LINE__, fmt, __VA_ARGS__);
+#define LOG_CRITICAL_SHORT(log, fmt, ...)                                      \
         internal_logger_short(log, LEVEL_CRITICAL, fmt, __VA_ARGS__);
 
 #endif /* CLOG_H */
--- src/clog.c
+++ src/clog.c
@@ -1,8 +1,86 @@
+#include <pthread.h>
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "clog.h"
 
-int main(void)
+struct logger *create_logger(void)
 {
-        log("Hello world");
+        struct logger *logger = malloc(sizeof(struct logger));
+
+        if (logger == NULL) {
+                // TODO: Handle error
+                return NULL;
+        }
+
+        /* Add some sensible defaults */
+        logger->max_level = LEVEL_ERROR;
+        logger->error_handler_func = NULL;
+
+        pthread_mutex_t *lock = NULL;
+        pthread_mutex_init(lock, NULL);
+
+        logger->lock = lock;
+        logger->output_logger_func = NULL;
+        logger->quiet = false;
+
+        return logger;
+}
+
+void destroy_logger(struct logger *log)
+{
+        pthread_mutex_destroy(log->lock);
+
+        free(log);
+}
+
+void set_logging_max_level(struct logger *log, enum log_level level)
+{
+        log->max_level = level;
+}
+
+void set_error_func(struct logger *log, error_func_t error_handler_func)
+{
+        log->error_handler_func = error_handler_func;
+}
+
+void set_output_func(struct logger *log, output_func_t output_logger_func)
+{
+        log->output_logger_func = output_logger_func;
+}
+
+void set_quiet(struct logger *log, bool quiet)
+{
+        log->quiet = quiet;
+}
+
+void internal_logger(const struct logger *log, enum log_level level,
+                     const char *file, uint32_t line, const char *fmt, ...)
+{
+        if (level < log->max_level) {
+                return;
+        }
+
+        fprintf(stderr, "%s:%d", file, line);
+
+        va_list args;
+        va_start(args, fmt);
+        vfprintf(stderr, fmt, args);
+        va_end(args);
+        fprintf(stderr, "\n");
+}
+
+void internal_logger_short(const struct logger *log, enum log_level level,
+                           const char *fmt, ...)
+{
+        if (level < log->max_level) {
+                return;
+        }
+
+        va_list args;
+        va_start(args, fmt);
+        vfprintf(stderr, fmt, args);
+        va_end(args);
+        fprintf(stderr, "\n");
 }
