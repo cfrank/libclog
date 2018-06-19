e33c16a7ebfd496af1da2655fc147212ef48c69e
Chris Frank <chris@cfrank.org>
2018-06-19 15:07:37 -0400

    Add example to readme
    - Fix typo
    - Add example to readme

--- examples/simple.c
+++ examples/simple.c
@@ -2,12 +2,14 @@
 
 int main(void)
 {
-        struct logger *log = create_logger("SIMPLE");
+        struct logger *log = create_logger("MYLOGGER");
 
         LOG_ERROR(log, "An error has occured: %s", "Error Name");
         LOG_ERROR_SHORT(log, "A short error has occured: %s", log->name);
 
         LOG_ERROR(log, "An error has occured - literally");
+
+        LOG_ERROR(log, "Your %s", "Message");
 
         destroy_logger(log);
 }
